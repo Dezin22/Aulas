6d26689725e64e2e70fe13082186e7abbfc6c575
andre <andregroliveira@gmail.com>
2024-12-02 18:04:29 -0300

    aula 1 js

---
+++ JavaScript/Aula1/script.js
@@ -0,0 +1,28 @@
+alert('Ola pessoal')
+console.log('Ola pessoal')
+
+// var alerta = 'Atenção Pessoal'
+// let alerta2 = 'ANTEÇAOO PESSOALLL'
+// const alerta3 = 'Vai explodir'
+// alert(alerta)
+// alert(alerta2)
+// alert(alerta3)
+
+// let nome2 = prompt('Qual seu nome? ')
+// console.log(nome2)
+
+let num1 = prompt('escolha um numero: ')
+
+let num2 = prompt('escolha um numero: ')
+
+alert(Number(num1) + Number(num2))
+
+const titulo = document.createElement('h1')
+titulo.textContent = 'Pagina nova'
+document.body.appendChild(titulo)
+
+const paragrafo = document.createElement('p')
+paragrafo.textContent = 'Olá mundo'
+document.body.appendChild(paragrafo)
+paragrafo.style.color = 'red'
+
